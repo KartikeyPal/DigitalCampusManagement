da180dca935daa0ef911ed5981502be386ce1ceb
anuragjadoun <anuragjadoun024@gmail.com>
2026-01-23 17:51:29 +0530

    Testing by anurag new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <Route path='/' element={<HomePage/>}/>
         <Route path='*' element={<h1>404 Not Found</h1>}/>
         <Route path='/home' element={<HomePage/>}/>
+        <Route path='/about' element={<h1>About Page</h1>}/>
       </Routes>
     </>
   )
