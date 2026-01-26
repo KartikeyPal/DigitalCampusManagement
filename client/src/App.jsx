ec5c339df1c6211289d32b5e6f766cf4f8270881
= <omchavan2024@gmail.com>
2026-01-27 00:57:46 +0530

    created about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Route,Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
+import About from './pages/About.jsx'
+
 function App() {
 
   return (
@@ -12,7 +14,7 @@ function App() {
         <Route path='/login' element={<Signin/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='*' element={<h1>404 Not Found</h1>}/>
-        <Route path='/about' element={<h1>About Page</h1>}/>
+        <Route path='/about' element={<About />}/>
       </Routes>
     </>
   )
